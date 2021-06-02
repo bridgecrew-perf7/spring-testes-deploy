953438735ea29b1b96e3c1bd442de290b1b910d5
genildooppy <genildo.araujo@oppy.com.br>
2021-06-02 13:13:55 -0300

    02/06/2021

---
+++ spring-testes-deploy.bat
@@ -0,0 +1,1 @@
+cmd /c (start /b /d "E:\BKP_GENILDO_OPPY\WORKSPACES\workspace-kafka\spring-testes-deploy" "%cd%" "C:\Program Files\Git\git-bash.exe" -i -c "git add .;git commit -m "%date%"; git push https://github.com/genildoaraujo77/spring-testes-deploy.git master -f; exec bash") && pause
